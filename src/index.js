72fd68b02f167ccf0985dbaebe533a5397ee2be2
valerib099 <valeria.balderrama@cetys.edu.mx>
2021-03-23 01:48:08 -0700

    api done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
+const { json } = require('express');
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
 //configuraciones
-app.set('port',3000)
+app.set('port', 3000);
+app.set('json spaces',2);
 
 //middlewares
 app.use(morgan('dev'));
 app.use(express.json());
 
 //rutas
-app.use(require('./rutas/rutas'))
+app.use('/api/reportes',require('./rutas/rutas'));
 
 //empezando servidor
 app.listen(app.get('port'));
--- src/rutas/rutas.js
+++ src/rutas/rutas.js
@@ -1,12 +1,77 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const router = Router();
 
 //const express = require(express);
 //const express = express.Router();
 
 //rutas
-app.get('/', (req,res) =>{
-    res.send({"name" :"John Doe "});
+const reportes = require('./reportes.json');
+console.log(reportes);
+
+router.get('/', (req,res) => {
+    res.json(reportes);
+})
+
+router.get('/:fecha', (req,res) => {
+    const {fecha}= req.params;
+    reportes.forEach(reporte => {
+        if(reporte.fecha == fecha){ 
+        res.json(reporte);
+        }
+    });
 });
 
+router.get('/:fecha/impresiones/', (req,res) => {
+    const {fecha}= req.params;
+    reportes.forEach(reporte => {
+        if(reporte.fecha == fecha){
+        res.json(reporte.impresiones);
+        }
+    });
+});
+
+router.get('/:fecha/clickIndividuales/', (req,res) => {
+    const {fecha}= req.params;
+    reportes.forEach(reporte => {
+        if(reporte.fecha == fecha){
+        res.json(reporte.clickIndividuales);
+        }
+    });
+});
+
+router.get('/:fecha/tiempoNavegacion/', (req,res) => {
+    const {fecha}= req.params;
+    reportes.forEach(reporte => {
+        if(reporte.fecha == fecha){
+        res.json(reporte.tiempoNavegacion);
+        }
+    });
+});
+
+router.get('/:fecha/procesosCompletos/', (req,res) => {
+    const {fecha}= req.params;
+    reportes.forEach(reporte => {
+        if(reporte.fecha == fecha){
+        res.json(reporte.procesosCompletos);
+        }
+    });
+});
+
+router.get('/:fecha/Conversiones/', (req,res) => {
+    const {fecha}= req.params;
+    reportes.forEach(reporte => {
+        if(reporte.fecha == fecha){
+        res.json(reporte.Conversiones);
+        }
+    });
+});
+
+router.get('/:fecha/SalidaDeSitio/', (req,res) => {
+    const {fecha}= req.params;
+    reportes.forEach(reporte => {
+        if(reporte.fecha == fecha){
+        res.json(reporte.SalidaDeSitio);
+        }
+    });
+});
 module.exports = router;
